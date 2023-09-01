ac1e5fe450b48968aaed812272b3f13d14258ee6
inchan Tak <ict970@gmail.com>
2023-09-01 16:34:40 +0900

    삭제하기 및 페이징 추가

--- src/main/java/board/boardbuild/controller/BoardController.java
+++ src/main/java/board/boardbuild/controller/BoardController.java
@@ -4,6 +4,9 @@ import board.boardbuild.controller.dto.BoardDTO;
 import board.boardbuild.service.BoardService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +39,9 @@ public class BoardController {
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model){ //경로상의 값을 가져올때는 pathvariable을 사용한다
+    public String findById(@PathVariable Long id, Model model,
+            @PageableDefault(page = 1) Pageable pageable){
+        //경로상의 값을 가져올때는 pathvariable을 사용한다
         /*
         해당 게시글의 조회수를 하나 올리고
         게시글의 데이터를 가져와서 detail.html에 출력한다
@@ -44,6 +49,7 @@ public class BoardController {
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
+        model.addAttribute("page", pageable.getPageNumber());
         return "detail";
     }
     @GetMapping("/update/{id}")
@@ -60,6 +66,35 @@ public class BoardController {
         model.addAttribute("board", board);
         return "detail";
 //        return "redirect:/board/"+boardDTO.getId(); 이렇게 사용할 시 수정할때도 조회수가 올라감
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1)Pageable pageable, Model model){
+//        pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        // page 갯수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 갯수 3개
+        // 총 페이지 갯수 8개
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
+
     }
 
 
